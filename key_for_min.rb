21665e1a2b8e4b2e9373904fbd58d13cadc3478b
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-10 00:47:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
   min_value = 0
   the_key = nil
@@ -11,5 +10,5 @@ def key_for_min_value(name_hash)
       the_key = key
     end
   end
-  return the_key
+  the_key
 end
