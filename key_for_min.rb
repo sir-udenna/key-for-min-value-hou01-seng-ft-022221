5dca97a23e1706c018f67330553d0f83f0790e7c
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-10 00:46:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = 0
   the_key = nil
   name_hash.each do |key, value|
-    min value = value
+    min_value = value
     if min_value > value
       min_value = value
       the_key = key
